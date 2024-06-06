fc64284e0088c3eff9d97e94193a59dd79c091ad
Hugo Santos <145072624+guguim@users.noreply.github.com>
2024-06-06 16:04:53 -0300

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,7 @@
-import "./Home.scss"
-import Navbar from "../components/Navbar"
-import Forms from "../components/Forms"
-import Footer from "../components/Footer"
+import "./Home.scss";
+import Navbar from "../components/Navbar";
+import Forms from "../components/Forms";
+import Footer from "../components/Footer";
+
+import backgroung from "../../assets/backgroung.jpeg";
+import logo from "../../assets/logo.png";
