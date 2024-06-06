86350053842c2776ba370e4d70108afdaefa80ab
Hugo Santos <145072624+guguim@users.noreply.github.com>
2024-06-06 15:49:54 -0300

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,59 @@
+import "./Footer.scss"
+
+const Footer = () => {
+    return (
+      <footer>
+        <div id="footer-container">
+          <div id="footer-logo">
+            <img src={img} alt="" />
+            <p>HCM-01 All rights reserved</p>
+          </div>
+  
+          <ul className="footer-list">
+            <li>
+              <h3>Info</h3>
+            </li>
+            <li>
+              <a href="#form" className="footer-links">
+                Contact us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer-links">
+                Privacy Policies
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer-links">
+                Terms & Conditions
+              </a>
+            </li>
+          </ul>
+  
+          <ul className="footer-list">
+            <li>
+              <h3>Company</h3>
+            </li>
+            <li>
+              <a href="#form" className="footer-links">
+                Help
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer-links">
+                Careers
+              </a>
+            </li>
+            <li>
+              <a href="#banner" className="footer-links">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,4 @@
 import "./Home.scss"
 import Navbar from "../components/Navbar"
 import Forms from "../components/Forms"
+import Footer from "../components/Footer"
